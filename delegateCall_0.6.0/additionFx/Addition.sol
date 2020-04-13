227512d477db8d74efb397ac3d2a2e044a284b70
amateur_dev <me@dipeshsukhani.dev>
2020-04-13 14:30:27 +0800

    Adding local Contract Storage

--- delegateCall_0.6.0/additionFx/Addition.sol
+++ delegateCall_0.6.0/additionFx/Addition.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.6.0;
 
 contract addition {
+    uint public original_value;
     
-    function add(uint a, uint b) public pure returns (uint ans) {
-        return (a+b);
+    function add(uint a, uint b) public returns (uint ans) {
+        original_value = a+b;
+        return (original_value);
     }
 }
