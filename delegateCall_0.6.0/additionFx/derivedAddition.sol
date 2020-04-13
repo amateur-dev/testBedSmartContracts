f0bdcd074d1b5caceca90343aea6a1a7a929163f
amateur_dev <me@dipeshsukhani.dev>
2020-04-13 14:31:02 +0800

    Now emitting the data

--- delegateCall_0.6.0/additionFx/derivedAddition.sol
+++ delegateCall_0.6.0/additionFx/derivedAddition.sol
@@ -5,9 +5,12 @@ import './Addition.sol';
 contract derived_addition {
     addition public AddAddress;
     uint public value;
+    event data(bytes);
     
     function dele_add(uint a, uint b) public {
+        bytes memory random  = abi.encodeWithSignature("add(uint256,uint256)", a, b);
         (, bytes memory result) = address(AddAddress).delegatecall(abi.encodeWithSignature("add(uint256,uint256)", a, b));
+        emit data(random);
         value = abi.decode(result, (uint256));
     }
     
