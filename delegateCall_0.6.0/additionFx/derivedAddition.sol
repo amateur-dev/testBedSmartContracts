265dd72ceefb8fce1cf5c1b5388ed1b5f768ac99
amateur_dev <me@dipeshsukhani.dev>
2020-04-13 14:16:31 +0800

    Now with decoding the output from hex to uit

--- delegateCall_0.6.0/additionFx/derivedAddition.sol
+++ delegateCall_0.6.0/additionFx/derivedAddition.sol
@@ -4,10 +4,11 @@ import './Addition.sol';
 
 contract derived_addition {
     addition public AddAddress;
+    uint public value;
     
-    function dele_add(uint a, uint b) public returns (bytes memory deri_ans){
+    function dele_add(uint a, uint b) public {
         (, bytes memory result) = address(AddAddress).delegatecall(abi.encodeWithSignature("add(uint256,uint256)", a, b));
-        return result;
+        value = abi.decode(result, (uint256));
     }
     
     function setAddress(address _setAdditionAddres) public {
